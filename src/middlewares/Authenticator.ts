7c632483277fac45c949311ff9131209407abd76
JoaoPauloAAlvim <alvesalvimj@gmail.com>
2024-12-09 17:16:29 -0300

    Criação da classe Authenticator

--- src/middlewares/Authenticator.ts
+++ src/middlewares/Authenticator.ts
@@ -1,22 +1,25 @@
 import * as jwt from "jsonwebtoken";
 import authenticationData from "../types/typeAuthenticationData";
 import dotenv from "dotenv";
+import { payload } from "../types/typePayload";
 
 dotenv.config();
 
 export class Authenticator {
-  generateToken = (input: authenticationData): string => {
-    return jwt.sign(input, String(process.env.JWT_KEY), { expiresIn: "1d" });
+  generateToken = (payload: payload): string => {
+    return jwt.sign({ id: payload.userId,
+      role:payload.role
+     }, 
+     String(process.env.JWT_KEY), 
+     {expiresIn: "90m",}
+    );
   };
-  getTokenData = (token: string): authenticationData | null => {
-    try {
-      const { id } = jwt.verify(
+  verifyToken = (token: string): payload => {
+      const tokenData = jwt.verify(
         token,
         String(process.env.JWT_KEY)
-      ) as authenticationData;
-      return { id };
-    } catch (error) {
-      return null;
+      ) as payload;
+      return tokenData;
     }
   };
-}
+
